6c1a375bc2e62b4072f1527c5fd4fb4f1f5ae7ef
KayoFontesLunga <kayolunga84@gmail.com>
2025-03-06 16:43:03 -0300

    :sparkles: feat: Endpoint for adding a product to the cart

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ShoppingCartWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataBaseHelper _dbHelper;
+        public ProductController(DataBaseHelper dbHelper) 
+        {
+            _dbHelper = dbHelper;
+        }
+        public async Task<IActionResult> AddProduct([FromQuery] string productName, double price)
+        {
+            string query = "INSERT INTO Product (ProductName, Price) VALUES (@ProductName, @Price)";
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@ProductName", productName),
+                new SqlParameter("@Price", price)
+            };
+            await _dbHelper.ExecuteNonQueryAsync(query, parameters);
+            return Ok($"{productName} added to cart");
+        }
+    }
+}
