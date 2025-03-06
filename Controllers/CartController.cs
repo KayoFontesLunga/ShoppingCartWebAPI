e5dc259fe7f43d5ba9d0574d3fe5ae62abacd809
KayoFontesLunga <kayolunga84@gmail.com>
2025-03-05 22:44:21 -0300

    :sparkles: feat: First API endpoint

---
+++ Controllers/CartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ShoppingCartWebAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly DataBaseHelper _dbHelper;
+        public CartController(DataBaseHelper dbHelper) 
+        {
+            _dbHelper = dbHelper;
+        }
+
+        [HttpPost]
+        [Route("Add")]
+        public async Task<IActionResult> AddCart([FromQuery] string userName)
+        {
+            string query = "INSERT INTO Cart (UserName) VALUES (@UserName)";
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@UserName", userName)
+            };
+            await _dbHelper.ExecuteNonQueryAsync(query, parameters);
+            return Ok($"{userName} added to cart");
+        }
+    }
+}
+    
---
+++ DataBaseHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace ShoppingCartWebAPI;
+
+public class DataBaseHelper
+{
+    private readonly string _connectionString;
+    public DataBaseHelper(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DataBase");
+    }
+    public async Task<DataTable> ExecuteQueryAsync(string query, SqlParameter[] parameters = null)
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+    }
+    public async Task<int> ExecuteNonQueryAsync(string query, SqlParameter[] parameters = null)
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+                return await command.ExecuteNonQueryAsync();
+            }
+        }
+    }
+}
+
