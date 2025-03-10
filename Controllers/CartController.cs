a4d5c37b7a214ad294409679d1c043aecbb9c785
KayoFontesLunga <kayolunga84@gmail.com>
2025-03-09 22:32:17 -0300

    :bug: fix: Fixing delete endpoint error

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -20,79 +20,101 @@ namespace ShoppingCartWebAPI.Controllers
         [Route("Add")]
         public async Task<IActionResult> AddCart([FromQuery] string userName)
         {
-            string query = "INSERT INTO Cart (UserName) VALUES (@UserName)";
-            SqlParameter[] parameters =
+            try
             {
-                new SqlParameter("@UserName", userName)
-            };
-            await _dbHelper.ExecuteNonQueryAsync(query, parameters);
-            return Ok($"{userName} added to cart");
+                string query = "INSERT INTO Cart (UserName) VALUES (@UserName)";
+                SqlParameter[] parameters =
+                {
+                    new SqlParameter("@UserName", userName)
+                };
+                await _dbHelper.ExecuteNonQueryAsync(query, parameters);
+                return Ok($"{userName} added to cart");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
         }
         [HttpPost]
         [Route("AddItemToCart")]
         public async Task<IActionResult> AddItemToCart([FromQuery] int cartId, int productId, int quantity)
         {
-            var selectQuery = "SELECT Quantity FROM Cart_Has_Product WHERE CartId = @CartId AND ProductId = @ProductId";
-            SqlParameter[] selectParameters =
+            try
             {
-                new SqlParameter("@CartId", cartId),
-                new SqlParameter("@ProductId", productId),
-            };
-
-            object result = await _dbHelper.ExecuteScalarAsync(selectQuery, selectParameters);
-            int existingQuantity = (result != null && result != DBNull.Value) ? Convert.ToInt32(result) : 0;
-
-            if (existingQuantity > 0)
-            {
-                string updateQuery = "UPDATE Cart_Has_Product SET Quantity = Quantity + @Quantity WHERE CartId = @CartId AND ProductId = @ProductId";
-                SqlParameter[] updateParameters =
+                var selectQuery = "SELECT Quantity FROM Cart_Has_Product WHERE CartId = @CartId AND ProductId = @ProductId";
+                SqlParameter[] selectParameters =
                 {
-                   new SqlParameter("@CartId", cartId),
-                   new SqlParameter("@ProductId", productId),
-                   new SqlParameter("@Quantity", quantity + 1)
+                    new SqlParameter("@CartId", cartId),
+                    new SqlParameter("@ProductId", productId),
                 };
-                await _dbHelper.ExecuteNonQueryAsync(updateQuery, updateParameters);
-                return Ok($"Item quantity updated: {existingQuantity} ➝ {existingQuantity + 1}");
-            }
-            else
-            {
-                string query = "INSERT INTO Cart_Has_Product (CartId, ProductId, Quantity) VALUES (@CartId, @ProductId, @Quantity)";
-                SqlParameter[] parameters =
+
+                object result = await _dbHelper.ExecuteScalarAsync(selectQuery, selectParameters);
+                int existingQuantity = (result != null && result != DBNull.Value) ? Convert.ToInt32(result) : 0;
+
+                if (existingQuantity > 0)
                 {
-                new SqlParameter("@CartId", cartId),
-                new SqlParameter("@ProductId", productId),
-                new SqlParameter("@Quantity", quantity > 0 ? quantity : 1)
-            };
-                await _dbHelper.ExecuteNonQueryAsync(query, parameters);
-                return Ok("New Item added to cart");
+                    string updateQuery = "UPDATE Cart_Has_Product SET Quantity = Quantity + @Quantity WHERE CartId = @CartId AND ProductId = @ProductId";
+                    SqlParameter[] updateParameters =
+                    {
+                        new SqlParameter("@CartId", cartId),
+                        new SqlParameter("@ProductId", productId),
+                        new SqlParameter("@Quantity", quantity + 1)
+                    };
+                    await _dbHelper.ExecuteNonQueryAsync(updateQuery, updateParameters);
+                    return Ok($"Item quantity updated: {existingQuantity} ➝ {existingQuantity + 1}");
+                }
+                else
+                {
+                    string query = "INSERT INTO Cart_Has_Product (CartId, ProductId, Quantity) VALUES (@CartId, @ProductId, @Quantity)";
+                    SqlParameter[] parameters =
+                    {
+                        new SqlParameter("@CartId", cartId),
+                        new SqlParameter("@ProductId", productId),
+                        new SqlParameter("@Quantity", quantity > 0 ? quantity : 1)
+                    };
+                    await _dbHelper.ExecuteNonQueryAsync(query, parameters);
+                    return Ok("New Item added to cart");
+                }
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "Internal server error: " + ex.Message);
             }
         }
         [HttpDelete]
         [Route("DeleteItemFromCart")]
         public async Task<IActionResult> DeleteItemFromCart([FromQuery] int cartId, int productId)
         {
-            var selectQuery = "SELECT Quantity FROM Cart_Has_Product WHERE CartId = @CartId AND ProductId = @ProductId";
-            SqlParameter[] selectParameters =
+            try
             {
-                new SqlParameter("@CartId", cartId),
-                new SqlParameter("@ProductId", productId),
-            };
-            object result = await _dbHelper.ExecuteNonQueryAsync(selectQuery, selectParameters);
-            int existingQuantity = (result != null && result != DBNull.Value) ? Convert.ToInt32(result) : 0;
-            if(existingQuantity > 0)
-            {
-                string query = "DELETE FROM Cart_Has_Product WHERE CartId = @CartId AND ProductId = @ProductId";
-                SqlParameter[] parameters =
+                var selectQuery = "SELECT Quantity FROM Cart_Has_Product WHERE CartId = @CartId AND ProductId = @ProductId";
+                SqlParameter[] selectParameters =
                 {
-                new SqlParameter("@CartId", cartId),
-                new SqlParameter("@ProductId", productId),
+                    new SqlParameter("@CartId", cartId),
+                    new SqlParameter("@ProductId", productId),
                 };
-                await _dbHelper.ExecuteNonQueryAsync(query, parameters);
-                return Ok("Item deleted from cart");
+                object result = await _dbHelper.ExecuteScalarAsync(selectQuery, selectParameters);
+                int existingQuantity = (result != null && result != DBNull.Value) ? Convert.ToInt32(result) : 0;
+
+                if (existingQuantity > 0)
+                {
+                    string query = "DELETE FROM Cart_Has_Product WHERE CartId = @CartId AND ProductId = @ProductId";
+                    SqlParameter[] parameters =
+                    {
+                        new SqlParameter("@CartId", cartId),
+                        new SqlParameter("@ProductId", productId),
+                    };
+                    await _dbHelper.ExecuteNonQueryAsync(query, parameters);
+                    return Ok("Item deleted from cart");
+                }
+                else
+                {
+                    return BadRequest("Item not found in cart");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return BadRequest("Item not found in cart");
+                return StatusCode(500, "Internal server error: " + ex.Message);
             }
         }
     }
